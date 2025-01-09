66d4454e8530f027ae724b3e8c3551e43174afb9
Piratheep <rpiratheep2002@gmail.com>
2025-01-09 19:43:55 +0530

    add

--- Practice_01/SumNumbers.java
+++ Practice_01/SumNumbers.java
@@ -5,14 +5,20 @@ public class SumNumbers
     {
         Scanner sumnumbers = new Scanner(System.in);
 
+        // Prompt the user to enter two numbers and store them
+
+        // Get the first number from the user
         System.out.print("Enter number1: ");
         int num1 = sumnumbers.nextInt();
 
-
+        // Get the second number from the user
         System.out.print("Enter number2: ");
         int num2 = sumnumbers.nextInt();
 
+
+        // Calculate the sum of the two numbers and display it to the user
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
-    }
+        }
 }
+
